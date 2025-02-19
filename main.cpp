ae32f72f0778d4ce70ceb7b00aa75868fdaa47d1
IlyaBfb <ibarashkov15@gmail.com>
2025-02-19 19:36:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
-Код с 1 сема
+#include <iostream>
+using namespace std;
+long long gcd(long long a,long long b){
+    while (a && b)
+        if (a > b) a%=b;
+        else b%=a;
+    return a+b;}
+int main() {
+    long long numb1, numb2;
+    cin >> numb1 >> numb2;
+    cout << gcd(numb1, numb2) << endl;
+    return 0;}
